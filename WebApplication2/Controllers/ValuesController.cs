1504401a266a31915dd3a7d3231dd9bdc34da911
Даша и Вова <nikolaevs@MBP-Dasa.Dlink>
2019-07-05 01:47:49 +0300

    First commit

---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IUserStorage userStorage;
+
+        public ValuesController(IUserStorage storage)
+        {
+            userStorage = storage;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return userStorage.Instance.Values.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            User user;
+            userStorage.Instance.TryGetValue(id, out user);
+
+            return user;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] User user)
+        {
+            if (userStorage.Instance.ContainsKey(user.Id))
+            {
+                return BadRequest();
+            }
+            else
+            {
+                userStorage.Instance.Add(user.Id, user);
+                return Ok();
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            if (userStorage.Instance.ContainsKey(id))
+            {
+                userStorage.Instance[id] = new User(id, value);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (userStorage.Instance.ContainsKey(id))
+            {
+                userStorage.Instance.Remove(id);
+                return this.Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ WebApplication2/IStorage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace WebApplication2
+{
+    public interface IUserStorage
+    {
+        IDictionary<int, User> Instance { get; }
+        void addUser(User user);
+     
+    }
+
+    class UserStorage : IUserStorage
+    {
+       
+        private static IDictionary<int, User> instance = new ConcurrentDictionary<int, User>();
+
+        public IDictionary<int, User> Instance  => instance;
+
+
+        public void addUser(User user)
+        {
+            Instance.Add(user.Id, user);
+        }
+    }
+}
